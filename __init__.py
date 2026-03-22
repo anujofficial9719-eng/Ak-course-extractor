ca6e42becf11650860e0658a8e8e10f50ff5d139
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-22 20:00:30 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
-"""HTML generation module for text to HTML conversion."""
+import glob
+from os.path import basename, dirname, isfile
 
-from .bot import handle_txt2html, show_txt2html_help
 
-__all__ = ['handle_txt2html', 'show_txt2html_help']
+def __list_all_modules():
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
